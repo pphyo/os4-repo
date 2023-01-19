e5213173a23bf545e8fd732298faa8fd6ed46d18
pphyo <pyaephyo.jdc@gmail.com>
2023-01-19 12:06:52 +0630

    Nested Class and Design Pattern

---
+++ pattern03-builder/src/com/jdc/build/BuilderApp.java
@@ -0,0 +1,34 @@
+package com.jdc.build;
+
+public class BuilderApp {
+
+	public static void main(String[] args) {
+		
+		ItemBuilder builder = new ItemBuilder();
+		
+		Item item = builder.createItem()
+						   .addQuantity(10)
+						   .addName("Royal")
+						   .addStock(true)
+						   .addPrice(40000.546)
+						   .addQuantity(2)
+						   .build();
+		
+		String result = """
+				%-8s: %s
+				%-8s: %.2f
+				%-8s: %d
+				%-8s: %s
+				%-8s: %s
+				""".formatted("Name", item.getName(),
+						"Price", item.getPrice(),
+						"Quantity", item.getQuantity(),
+						"Stock", item.isStock(),
+						"Grade", item.getGrade()
+						);
+		
+		System.out.println(result);
+		
+	}
+
+}
