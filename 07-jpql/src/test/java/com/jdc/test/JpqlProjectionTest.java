695dc69304316fdf4060dcbc9a50136f0a7ebbbf
pphyo <pyaephyo.jdc@gmail.com>
2023-02-23 11:49:15 +0630

    group by having in jpql

--- 07-jpql/src/test/java/com/jdc/test/JpqlProjectionTest.java
+++ 07-jpql/src/test/java/com/jdc/test/JpqlProjectionTest.java
@@ -2,16 +2,19 @@ package com.jdc.test;
 
 import org.junit.jupiter.api.Test;
 
+import com.jdc.app.model.dto.SaleOrderCountbyProduct;
+
 public class JpqlProjectionTest extends AbstractTest {
 
 	@Test
 	void test() {
 		String jpql = """
-				select s.name, p.name, so.id, so.product.id
-				from Supplier s join s.products p join SALE_ORDER so
+				select new com.jdc.app.model.dto.SaleOrderCountbyProduct(s.name, so.product.name, sum(so))
+				from SALE_ORDER so join so.product.suppliers s
+				group by s.name, so.product.name order by so.product.name desc
 				""";
 		
-		var query = em.createQuery(jpql, Object[].class);
+		var query = em.createQuery(jpql, SaleOrderCountbyProduct.class);
 		
 		query.getResultList().stream().forEach(System.out::println);
 	}
